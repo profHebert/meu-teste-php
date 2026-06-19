564fc7b8707daa2b0b222653d60cbaa9227ec065
Hebert Bratefixe Alquimim <hebert@uni9.pro.br>
2026-06-19 11:05:19 -0300

    include_config

--- api/admin_login.php
+++ api/admin_login.php
@@ -4,11 +4,11 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 // Suas definições diretas que funcionaram perfeitamente
-// include_once "config.php";
-$supabase_url = "https://vxkxptbrfbqygpisggjm.supabase.co";
-$supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4a3hwdGJyZmJxeWdwaXNnZ2ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMzkxMjUsImV4cCI6MjA5NjYxNTEyNX0.dEW3a_-Tgr-ufM3LLtzx1cuX1G4rMC_uK8lsJruGYt0";
-define('SUPABASE_URL', $supabase_url);
-define('SUPABASE_KEY', $supabase_key);
+include_once "config.php";
+// $supabase_url = "https://vxkxptbrfbqygpisggjm.supabase.co";
+// $supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4a3hwdGJyZmJxeWdwaXNnZ2ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMzkxMjUsImV4cCI6MjA5NjYxNTEyNX0.dEW3a_-Tgr-ufM3LLtzx1cuX1G4rMC_uK8lsJruGYt0";
+// define('SUPABASE_URL', $supabase_url);
+// define('SUPABASE_KEY', $supabase_key);
 $erro_login = '';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['acao_login'])) {
